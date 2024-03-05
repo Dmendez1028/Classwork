d273397abd8e47cc9c47e44c73138fa1aaae1ca7
Destiny <dmendez14@bostonk12.org>
2024-03-05 09:23:02 -0500

    python

---
+++ Python/integer_input2.py
@@ -0,0 +1,4 @@
+x=int(input('Please enter your age :'))
+print("you are ", x , "years old!")
+age=int(input('Please enter a integer :'))
+print("In",age,"years you" +" will be", x + age, "years old"+"!")
--- Test.py
+++ Test.py
@@ -1,3 +1,3 @@
-grade=int(input('please enter a number:'))
+x=int(input('Please enter your age :'))
 if 75<=grade:
     print("75 is a passing grade")
