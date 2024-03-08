5a22639600397865836c81680f38e6e4786945b4
Destiny <dmendez14@bostonk12.org>
2024-03-08 10:50:06 -0500

    python

---
+++ Python/ Week_13/ 2024-03-07/ practice2.py
@@ -0,0 +1,13 @@
+# Destiny Mendez
+# 3/7/2024
+
+name = input("Please enter your name: ")
+food = input("please enter your favorite food :")
+song = input("Please enter your favortie song :")
+quote = input("Please enter your favorite quote :")
+print(f"Information about {name}:")
+print("*****")
+print(f"Favorite food : {food}")
+print(f"Favorite song :\"{song}\"")
+print(f"Favorite quote :\"{quote}\"")
+print(f"Summary: {name}'s favorite food is {food} their favorite song is \"{song}\" and their favorite quote is \"{quote}\" " )
