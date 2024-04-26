0b947a225cb53609eb604e54425370e029f249e5
Destiny <dmendez14@bostonk12.org>
2024-04-26 09:21:09 -0400

    python

---
+++ HTML/ Week_16/practice.py
@@ -0,0 +1,17 @@
+#Destiny Mendez
+#4/25/2024
+
+#this is how you make a empty list
+a=[]
+
+#this is how you creat a list with values
+b=[1,2,3]
+
+print(a)
+print(b)
+
+a+=["Adyren"]
+b+=[4]
+print(a)
+print(b)
+
