1f33eaa7fc785bb12f056e79743429b5f6d4caa3
Destiny <dmendez14@bostonk12.org>
2024-05-08 09:12:14 -0400

    python

---
+++ Week_17/5/7/2024/check.py
@@ -0,0 +1,30 @@
+#Destiny Mendez
+#5/7/2024
+
+#Creating An Empy List
+a=[]
+
+#Using the operator
+a+=[1]
+a+=[2]
+a+=[3]
+
+#Printing the list
+print(a)
+
+#Creating an empty list named B
+b=[]
+
+#using the function to add to the list b
+b.append(1)
+b.append(2)
+b.append(3)
+
+#printing the list b
+print(b)
+
+#positional comparison
+print(a==b)
+
+#object comparison
+print(a is b)
