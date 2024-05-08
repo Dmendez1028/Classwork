accc1f80d513b346d7bb179607b0326d6ed2a568
Destiny <dmendez14@bostonk12.org>
2024-05-08 10:04:43 -0400

    python

---
+++ Week_17/24/5/08/Lists.py
@@ -0,0 +1,12 @@
+#Destiny Mendez
+#05/08/2024
+
+#creating an empty list
+lst=[]
+
+#Using a forloop to add the numbers 1-100 to the list
+for b in range(1,100):
+    lst.append(b)
+
+#printing the list lst
+print(lst)
